4f94768280b8aeb5833cf80bca107fddbce15563
Kief Morris <kmorris@thoughtworks.com>
2021-09-15 12:50:30 +0100

    Added terraform fmt to make validate, and changed formatting of the source to make fmt happy.

--- website-stack-monolithic/src/tf_backend.tf
+++ website-stack-monolithic/src/tf_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = var.terraform_cloud_organization
     workspaces {
       name = "spin-${var.instance_id}-s3_bucket-${var.bucket_name}"
