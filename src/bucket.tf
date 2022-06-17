a6a1cc8bb3edac08ef2a09c4c2dcce9615f25e76
Kief Morris <kmorris@thoughtworks.com>
2022-06-17 14:58:13 +0100

    Updated to use current s3 website bucket resource rather than one that has been deprecated

--- src/bucket.tf
+++ src/bucket.tf
@@ -1,23 +1,13 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = local.bucket_name
-  acl    = "public-read"
-  policy = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${local.bucket_name}/*"]
-    }
-  ]
-}
-POLICY
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+  bucket = local.bucket_name
+
+  index_document {
+    suffix = "index.html"
   }
+
+  error_document {
+    key = "error.html"
+  }
+
 }
