faad9717109c73168db8e32a9a737a754f20da13
Kief Morris <kmorris@thoughtworks.com>
2022-10-10 18:09:04 +0100

    Added instance configuration for a content staging site.

--- src/dns.tf
+++ src/dns.tf
@@ -23,16 +23,3 @@ resource "aws_route53_record" "www" {
 
   records = [local.website_hostname]
 }
-
-# resource "aws_route53_record" "base" {
-#   zone_id = aws_route53_zone.zone.zone_id
-#   name    = "${local.website_hostname}."
-#   type    = "A"
-
-#   alias {
-#     name                   = aws_cloudfront_distribution.site.domain_name
-#     zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
-
